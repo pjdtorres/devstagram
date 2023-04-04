2ed2f5806dc32abe41461ed7597aa01ba7dd7a54
pjdtorres <paulo.torres@ipcb.pt>
2023-04-04 18:49:44 +0100

    v1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,23 +4,40 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <title>DevStagram - @yield('titulo')</title>
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     </head>
-    <body>
-        <h1 class="text-4xl font-extrabold">@yield('titulo')</h1>
+    <body class="bg-gray-100">
+        <header class="p-5 bg-white border-b-8 shadow">
+            
+            <div class="container flex items-center justify-between mx-auto">
+                <h1 class="text-3xl font-black">
+                    Devstagram
+                </h1>
 
+                <nav class="flex items-center gap-2">
+                    <a class="text-sm font-bold text-gray-600 uppercase" href="#">Login</a>
+                    <a class="text-sm font-bold text-gray-600 uppercase" href="#">Criar Conta</a>
+                </nav>
+            </div>
+        </header>
 
-        <hr>
+        <main class="container mx-auto mt-10">
+            <h2 class="mb-10 text-3xl font-black text-center">
+                @yield('titulo')
+            </h2>
+            @yield('conteudo')
+        </main>
 
-        <h1 class="text-">treta</h1>
+        <footer class="p-5 font-bold text-center text-gray-500 uppercase">
+            Devstagram - Todos os direitos reservados @php echo date('Y'); @endphp
+        </footer>
+        <?php echo date('Y'); ?>
 
-        <hr>
-        <h2 class="font-black text-center text-3xl mb-10">
-            @yield('titulo')
-        </h2>
-        <hr>
-        @yield('conteudo')
+        {{ now()->year }}
     </body>
 </html>
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Página Principal
+@endsection
+
+@section('conteudo')
+    Conteudo desta Página
+@endsection
